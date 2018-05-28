907a24ad6e11f4f953d2a8e8303123874e92f10c
vCola <hongjunliu1989@163.com>
2018-05-28 19:46:25 +0800

    keep

---
+++ leetcode/src/test/java/com/vcola/leetcode/easy/CountAndSayTest.java
@@ -0,0 +1,15 @@
+package com.vcola.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CountAndSayTest {
+	
+	private CountAndSay countAndSay = new CountAndSay();
+
+	@Test
+	public void testCountAndSay() {
+		Assert.assertEquals("111221", countAndSay.countAndSay(5));
+	}
+
+}
