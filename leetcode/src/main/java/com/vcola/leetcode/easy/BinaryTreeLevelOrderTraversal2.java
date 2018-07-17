c5eca8ed3eb8727526c49575abf4fb1acc891a49
vCola <hongjunliu1989@163.com>
2018-07-17 09:44:55 +0800

    107

--- leetcode/src/main/java/com/vcola/leetcode/easy/BinaryTreeLevelOrderTraversal2.java
+++ leetcode/src/main/java/com/vcola/leetcode/easy/BinaryTreeLevelOrderTraversal2.java
@@ -1,5 +1,8 @@
 package com.vcola.leetcode.easy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -24,13 +27,59 @@ public class BinaryTreeLevelOrderTraversal2 {
 		  ]
 	 */
 	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<>();
+		if(root == null){
+			return result;
+		}
 		
-
-
+		LinkedList<TreeNode> preHeap = new LinkedList<>();
+		LinkedList<TreeNode> heap = new LinkedList<>();
+		preHeap.addFirst(root);
 		
-		
+		List<Integer> temp = new ArrayList<>();
+		while(!preHeap.isEmpty()){
+			TreeNode tempTreeNode = preHeap.removeLast();
+			temp.add(tempTreeNode.val);
+			
+			if(tempTreeNode.left != null){
+				heap.addFirst(tempTreeNode.left);
+			}
+			
+			if(tempTreeNode.right != null){
+				heap.addFirst(tempTreeNode.right);
+			}
+			
+			if(preHeap.isEmpty()){
+				result.add(temp);
+				temp = new ArrayList<>();
+				preHeap = heap;
+				heap = new LinkedList<>();
+			}
+		}
 
-		return null;
+		Collections.reverse(result);
+		return result;
 	}
+	
+	
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(3);
+		TreeNode left01 = new TreeNode(9);
+		TreeNode right01 = new TreeNode(20);
+		
+		TreeNode left02 = new TreeNode(15);
+		TreeNode right02 = new TreeNode(7);
+		
+		root.left = left01;
+		root.right = right01;
+		
+		right01.left = left02;
+		right01.right = right02;
+		
+		List<List<Integer>> result = new BinaryTreeLevelOrderTraversal2().levelOrderBottom(root);
+		System.out.println(result);
+		
+	}
+	
 
 }
