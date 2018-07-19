c216837afa52cc60821475582fa5f5776e118b9b
vCola <hongjunliu1989@163.com>
2018-07-19 09:37:07 +0800

    110

--- leetcode/src/main/java/com/vcola/leetcode/easy/BalancedBinaryTree.java
+++ leetcode/src/main/java/com/vcola/leetcode/easy/BalancedBinaryTree.java
@@ -1,5 +1,7 @@
 package com.vcola.leetcode.easy;
 
+import com.vcola.leetcode.easy.MaximumDepthOfBinaryTree.TreeNode;
+
 /**
  * 110. 平衡二叉树
  * 
@@ -7,9 +9,63 @@ package com.vcola.leetcode.easy;
  * @date 2018年7月18日 上午9:33:58
  */
 public class BalancedBinaryTree {
-	
+
 	public boolean isBalanced(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+
+		return isBalancedRecursion(root);
+	}
+
+	private boolean isBalancedRecursion(TreeNode node) {
+		if(node == null){
+			return true;
+		}
+		
+		if (Math.abs(maxDepth(node.left, 0) - maxDepth(node.right, 0)) > 1) {
+			return false;
+		}
+
+		if (!isBalancedRecursion(node.left)) {
+			return false;
+		}
+
+		if (!isBalancedRecursion(node.right)) {
+			return false;
+		}
+		
 		return true;
 	}
 
+	private int maxDepth(TreeNode node, int depth) {
+		if (node == null) {
+			return depth;
+		}
+
+		int maxLeftDepth = maxDepth(node.left, depth + 1);
+
+		int maxRightDepth = maxDepth(node.right, depth + 1);
+
+		return Math.max(maxLeftDepth, maxRightDepth);
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(3);
+		TreeNode left01 = new TreeNode(9);
+		TreeNode right01 = new TreeNode(20);
+		
+		TreeNode left02 = new TreeNode(15);
+		TreeNode right02 = new TreeNode(7);
+		
+		root.left = left01;
+		root.right = right01;
+		
+		right01.left = left02;
+		right01.right = right02;
+		
+		System.out.println(new BalancedBinaryTree().isBalanced(root));
+		
+	}
+	
 }
