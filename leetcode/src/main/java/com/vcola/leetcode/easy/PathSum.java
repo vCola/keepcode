697eaa3df31ba2752eaa556659db757a6352ef6e
vCola <hongjunliu1989@163.com>
2018-07-20 09:31:30 +0800

    111

---
+++ leetcode/src/main/java/com/vcola/leetcode/easy/PathSum.java
@@ -0,0 +1,17 @@
+package com.vcola.leetcode.easy;
+
+/**
+ * 112. 路径总和
+ * 
+ * @author Very Cola
+ * @date 2018年7月20日 上午9:30:00
+ */
+public class PathSum {
+
+	public boolean hasPathSum(TreeNode root, int sum) {
+		
+		
+		return false;
+	}
+
+}
