bb39e9c049ff6c956351de8985e77bcfcce6ae43
vCola <hongjunliu1989@163.com>
2018-08-15 09:51:38 +0800

    141

--- leetcode/src/main/java/com/vcola/leetcode/easy/LinkedListCycle.java
+++ leetcode/src/main/java/com/vcola/leetcode/easy/LinkedListCycle.java
@@ -1,5 +1,8 @@
 package com.vcola.leetcode.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.vcola.leetcode.common.ListNode;
 
 /**
@@ -11,10 +14,22 @@ import com.vcola.leetcode.common.ListNode;
 public class LinkedListCycle {
 
 	public boolean hasCycle(ListNode head) {
+		if(head == null || head.next == null){
+			return false;
+		}
 		
-		
-
+		Set<ListNode> set = new HashSet<>();
+		while(head != null){
+			if(set.contains(head)){
+				return true;
+			} 
+			set.add(head);
+			head = head.next;
+		}
 		return false;
 	}
+	
+	
+	
 
 }
