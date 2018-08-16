c63f84f14eca61af8b7f18dd9f75c5b5e3e18f64
vCola <hongjunliu1989@163.com>
2018-08-16 18:07:06 +0800

    160

--- leetcode/src/main/java/com/vcola/leetcode/common/ListNode.java
+++ leetcode/src/main/java/com/vcola/leetcode/common/ListNode.java
@@ -10,4 +10,18 @@ public class ListNode {
 		val = x;
 		next = null;
 	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		ListNode node = this;
+		while(node != null){
+			sb.append(node.val);
+			node = node.next;
+			if(node != null){
+				sb.append("->");
+			}
+		}
+		return sb.toString();
+	}
 }
