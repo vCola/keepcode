11772ea804be6ee8f570998ea655c38cd92f461f
vCola <hongjunliu1989@163.com>
2019-07-30 15:33:09 +0800

    test format

--- leetcode/src/test/java/com/vcola/leetcode/easy/SearchInsertPositionTest.java
+++ leetcode/src/test/java/com/vcola/leetcode/easy/SearchInsertPositionTest.java
@@ -7,36 +7,34 @@ import com.vcola.leetcode.easy.seq01.SearchInsertPosition;
 
 public class SearchInsertPositionTest {
 
-	private SearchInsertPosition searchInsertPosition = new SearchInsertPosition(); 
-	
-	@Test
-	public void testSearchInsertHave() {
-		int[] nums = {1,3,5,6};
-		int target = 5;
-		Assert.assertEquals(2, searchInsertPosition.searchInsert(nums, target));
-	}
+  private SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
 
-	@Test
-	public void testSearchInsertNotHave() {
-		int[] nums = {1,3,5,6};
-		int target = 2;
-		Assert.assertEquals(1, searchInsertPosition.searchInsert(nums, target));
-	}
-	
-	
-	@Test
-	public void testSearchInsertNotHave01() {
-		int[] nums = {1,3,5,6};
-		int target = 7;
-		Assert.assertEquals(4, searchInsertPosition.searchInsert(nums, target));
-	}
-	
-	
-	@Test
-	public void testSearchInsertNotHave02() {
-		int[] nums = {3, 5, 7, 9, 10};
-		int target = 8;
-		Assert.assertEquals(3, searchInsertPosition.searchInsert(nums, target));
-	}
-	
+  @Test
+  public void testSearchInsertHave() {
+    int[] nums = { 1, 3, 5, 6 };
+    int target = 5;
+    Assert.assertEquals(2, searchInsertPosition.searchInsert(nums, target));
+  }
+
+  @Test
+  public void testSearchInsertNotHave() {
+    int[] nums = { 1, 3, 5, 6 };
+    int target = 2;
+    Assert.assertEquals(1, searchInsertPosition.searchInsert(nums, target));
+  }
+
+  @Test
+  public void testSearchInsertNotHave01() {
+    int[] nums = { 1, 3, 5, 6 };
+    int target = 7;
+    Assert.assertEquals(4, searchInsertPosition.searchInsert(nums, target));
+  }
+
+  @Test
+  public void testSearchInsertNotHave02() {
+    int[] nums = { 3, 5, 7, 9, 10 };
+    int target = 8;
+    Assert.assertEquals(3, searchInsertPosition.searchInsert(nums, target));
+  }
+
 }
